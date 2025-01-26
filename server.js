db36564c8d6fc5ca77614042da4141bd325875d4
John Binze Escol <escoljohnbinze@gmail.com>
2025-01-27 02:42:56 +0800

    Revert back.

--- server.js
+++ server.js
@@ -721,6 +721,4 @@ server.on('upgrade', (request, socket, head) => {
   }
 });
 
-server.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on port ${PORT}`);
-});
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
